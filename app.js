53af1f80431250cbd7e242c31b0113b195a5eaee
s0loparty <izovo@bk.ru>
2022-03-02 21:18:47 +0300

    create cell's by js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const socket = io(location.origin.replace('5500', '5000'))
 
 document.addEventListener('DOMContentLoaded', () => {
+	const UUID = uuidv4() 
+
+	createCell()
+
 	const PLAYER_ITEMS = document.querySelectorAll('.space__box--player .space__fight-item')
 	const ENEMY_ITEMS = document.querySelectorAll('.space__box--enemy .space__fight-item')
-	const UUID = uuidv4() 
 
 	PLAYER_ITEMS.forEach((item, idx) => {
 		const cellId = idx + 1
@@ -25,6 +28,22 @@ document.addEventListener('DOMContentLoaded', () => {
 	})
 })
 
+function createCell() {
+	const listEnemy = document.querySelector('.space__box--enemy .space__fight-list')
+	const listPlayer = document.querySelector('.space__box--player .space__fight-list')
+	
+	for (let index = 0; index < 100; index++) {
+		const cell = document.createElement('div')
+		cell.classList.add('space__fight-item')
+		listEnemy.append(cell)
+	}
+
+	for (let index = 0; index < 100; index++) {
+		const cell = document.createElement('div')
+		cell.classList.add('space__fight-item')
+		listPlayer.append(cell)
+	}
+}
 function showShot(cellId, type = 'enemy') {
 	const classes = ['space__fight-item--miss', 'space__fight-item--damage', 'space__fight-item--killed']
 	const target = document.querySelector(`.space__fight-item[data-cell-${type}-id="${cellId}"]`)
